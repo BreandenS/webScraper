2ab31f6f3b60b457ff2042148de105568646ae2b
BreandenS <162747829+BreandenS@users.noreply.github.com>
2024-07-24 15:40:36 +0200

    Updated the README.md and changed the url to be scraped

--- webScraper.py
+++ webScraper.py
@@ -1,14 +1,12 @@
 import requests
-
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-url = "https://www.zdf.de/"
+url = "https://www.bbc.com/news"
 
 response = requests.get(url)
-
 # print(response)
 
 soup = BeautifulSoup(response.text, "lxml")
-print(soup)
+#print(soup)
